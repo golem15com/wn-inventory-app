c5c28c32d380075e6edf6d17ff9fb6ef2352a686
Jakub Zych <jakub@keios.eu>
2026-01-13 13:12:40 +0100

    Remove hardcoded winter plugins

--- plugins/winter/debugbar/lang/ru/lang.php
+++
@@ -1,8 +0,0 @@
-<?php return [
-    'plugin' => [
-        'name'                   => 'Панель отладки',
-        'description'            => 'Интегрирует отладочную панель PHP с Winter CMS',
-        'access_debugbar'        => 'Доступ к панели отладки',
-        'access_stored_requests' => 'Доступ к сохраненным запросам (рисковано)',
-    ],
-];
